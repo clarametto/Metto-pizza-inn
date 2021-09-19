2965da4a0600a6c3779edb442a068dbad87d434a
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-09-19 03:15:54 +0300

    added a JQuerry effect on class main

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $(".main").click(function(){
+        $(this).children("#design-p").slideToggle(1500);
+        $(this).children("#design").toggle();
+    });
+
+  
+
+
+    
+});
