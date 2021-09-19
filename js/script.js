70a38cf6a9d4572fad8dc96de15adfe943da39f3
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-09-19 04:14:17 +0300

    modified about.html and script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
     $(".main").click(function(){
-        $(this).children("#design-p").slideToggle(1500);
-        $(this).children("#design").toggle();
+        $(this).children("#design-p").slideToggle(2000);
+        $(this).children("#design").toggle(2000);
     });
 
   
