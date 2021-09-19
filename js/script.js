9673c2cac53756c89f5e70e3f0b208be2e9fc783
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-09-19 04:47:59 +0300

    added a a return function with if statements and mail cimp on conttact form

--- js/script.js
+++ js/script.js
@@ -5,6 +5,29 @@ $(document).ready(function(){
         $(this).children("#design").toggle(2000);
     });
 
+    // form
+
+    $(".form").submit(function(){
+        var name = document.getElementById("mce-NAME").value
+        var email = document.getElementById("mce-EMAIL").value
+        var message = document.getElementById("mce-MESSAGE").value
+        if (name == "" || name == null){
+            alert("Name can not be empty")
+        }
+        else if (email == "" || email == null){
+            alert("Email can not be empty")
+        }
+
+        else if (message == "" || message == null){
+            alert("message can not be empty")
+        }
+       else{
+        alert (" We have received your message " + name + ". Thank you for reaching out to us")
+       }
+        
+
+    })
+
   
 
 
