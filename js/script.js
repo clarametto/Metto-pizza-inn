b175c83ae706cf84f3ac726e1fa3f6d481156678
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-09-19 22:48:31 +0300

    modified order.html script.js and added a mailchip form action

--- js/script.js
+++ js/script.js
@@ -1,33 +1,29 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $(".main").click(function(){
+    $(".main").click(function () {
         $(this).children("#design-p").slideToggle(2000);
         $(this).children("#design").toggle(2000);
     });
 
     // form
 
-    $(".form").submit(function(){
+    $(".form").submit(function () {
         var name = document.getElementById("mce-NAME").value
         var email = document.getElementById("mce-EMAIL").value
         var message = document.getElementById("mce-MESSAGE").value
-        if (name == "" || name == null){
+        if (name == "" || name == null) {
             alert("Name can not be empty")
-        }
-        else if (email == "" || email == null){
+        } else if (email == "" || email == null) {
             alert("Email can not be empty")
+        } else if (message == "" || message == null) {
+            alert("message can not be empty")
+        } else {
+            alert(" We have received your message " + name + ". Thank you for reaching out to us")
         }
 
-        else if (message == "" || message == null){
-            alert("message can not be empty")
-        }
-       else{
-        alert (" We have received your message " + name + ". Thank you for reaching out to us")
-       }
-        
 
     })
-    
+
 });
 
 
@@ -36,193 +32,184 @@ function Pizza(size, crust, topping) {
     this.sizeValue = size;
     this.crustCost = crust;
     this.toppingCost = topping;
-  }
-  
-  Pizza.prototype.myOrder = function () {
+}
+
+Pizza.prototype.myOrder = function () {
     result = this.sizeValue + this.crustCost + this.toppingCost;
     return result;
-  
-  }
+
+}
 
 
-  //User Interface 
+//User Interface 
 
 $(document).ready(function () {
-    let orderForm = document.querySelector('#orderForm');
-    let checkOrder = document.querySelector("#checkOrder");
+    let formForOrder = document.querySelector('#formForOrder');
+    let orderCheck = document.querySelector("#orderCheck");
     let tot = document.querySelector("#total");
     let cart = [];
-    orderForm.addEventListener("submit", function (event) {
-      event.preventDefault();
-      $(this).closest('form').find("input[type=text], textarea").val("");
-      let size = $("#size").val();
-      let crust = $("#crust").val();
-      let topping = $("#topping").val();
-      let toppingCost = 0;
-      let crustCost = 0;
-      let sizeValue =0;
-      if (size.length === 0 || crust.length === 0 || topping.length === 0 ) {
-        alert("Select from all the fields before adding to cart!!! ")
-        throw new Error;
-      }
-      if (crust == "Crispy") {
-        crustCost = crustCost + 250;
-      } 
-      else if (crust == "Stuffed") {
-        crustCost = crustCost + 300;
-      } 
-      else if (crust == "Gluten-free") {
-        crustCost = crustCost + 350;
-      }
-  
-      if (topping == "Mushroom") {
-        toppingCost = toppingCost + 100;
-      } 
-      else if (topping == "Sausage") {
-        toppingCost = toppingCost + 150;
-      } 
-      else if (topping == "Green Pepper") {
-        toppingCost = toppingCost + 200;
-      }
-  
-      if (size == "Small") {
-        sizeValue += 200;
-      } 
-      else if (size == "Medium") {
-        sizeValue += 250;
-      } 
-      else if (size == "Large") {
-        sizeValue += 300;
-      }
-      let newOrder = new Pizza(sizeValue, crustCost, toppingCost);
-      let fd = new FormData(orderForm);
-      let order = {}
-  
-      for (let key of fd.keys()) {
-        if (fd.get(key).toString().length > 0) {
-          order[key] = fd.get(key).toString();
+    formForOrder.addEventListener("submit", function (event) {
+        event.preventDefault();
+        $(this).closest('form').find("input[type=text], textarea").val("");
+        let size = $("#size").val();
+        let crust = $("#crust").val();
+        let topping = $("#topping").val();
+        let toppingCost = 0;
+        let crustCost = 0;
+        let sizeValue = 0;
+        if (size.length === 0 || crust.length === 0 || topping.length === 0) {
+            alert("Select from all the fields before adding to cart!!! ")
+            throw new Error;
         }
-      }
-  
-      order.toppingCost = toppingCost;
-      order.crustCost = crustCost;
-      order.sizeValue = sizeValue;
-      order["total"] = (order["toppingCost"] + order["crustCost"] + order["sizeValue"]);
-      cart.push(order);
-      localStorage.setItem("cart", JSON.stringify(cart));
-      alert("Your selection have been successfuly added to cart.")
-      $(".view-order").show();
-      $("#guide").show();
-      let form1 =document.getElementsByName("orderForm")[0];
-      form1.reset();
+        if (crust == "Crispy") {
+            crustCost = crustCost + 250;
+        } else if (crust == "Stuffed") {
+            crustCost = crustCost + 300;
+        } else if (crust == "Gluten-free") {
+            crustCost = crustCost + 350;
+        }
+
+        if (topping == "Mushroom") {
+            toppingCost = toppingCost + 100;
+        } else if (topping == "Sausage") {
+            toppingCost = toppingCost + 150;
+        } else if (topping == "Green Pepper") {
+            toppingCost = toppingCost + 200;
+        }
+
+        if (size == "Small") {
+            sizeValue += 200;
+        } else if (size == "Medium") {
+            sizeValue += 250;
+        } else if (size == "Large") {
+            sizeValue += 300;
+        }
+        let newOrder = new Pizza(sizeValue, crustCost, toppingCost);
+        let fd = new FormData(formForOrder);
+        let order = {}
+
+        for (let key of fd.keys()) {
+            if (fd.get(key).toString().length > 0) {
+                order[key] = fd.get(key).toString();
+            }
+        }
+
+        order.toppingCost = toppingCost;
+        order.crustCost = crustCost;
+        order.sizeValue = sizeValue;
+        order["total"] = (order["toppingCost"] + order["crustCost"] + order["sizeValue"]);
+        cart.push(order);
+        localStorage.setItem("cart", JSON.stringify(cart));
+        alert("Your selection have been successfully added to cart.")
+        $(".viewYourOrder").show();
+        $("#guide").show();
+        let form1 = document.getElementsByName("formForOrder")[0];
+        form1.reset();
     });
 
 
-    checkOrder.addEventListener('click', function () {
+    orderCheck.addEventListener('click', function () {
         let cart = JSON.parse(localStorage.getItem("cart"));
         if (cart.length > 0) {
-          document.querySelector("#customerOrder").innerHTML = "";
-          cart.forEach(element => {
-          document.querySelector("#customerOrder").innerHTML += `<tr>
+            document.querySelector("#order").innerHTML = "";
+            cart.forEach(element => {
+                document.querySelector("#order").innerHTML += `<tr>
           <td>${element['size']}</td>
           <td>${element['crust']}</td>
           <td>${element['topping']}</td>
           <td>${(element['toppingCost']) + (element['crustCost']) + (element['sizeValue'])}</td>
           </tr>`;
-          });
+            });
         }
-    
+
         //print order summary to a table
 
-        $("#orderTable").show();
-        $("#ask").show();
+        $("#tableForOrder").show();
+        $("#location").show();
         const total = cart.reduce((sum, item) => sum + (parseInt(item['total'])), 0);
-        const shippingCost = 0.2*total;
+        const deliveryCost = 0.2 * total;
         tot.innerHTML = "Aggregate Order Price Ksh " + total.toString();
-    
-        //confirmation for delivery option
 
-        $("form#confirm").submit(function(event) {
-          event.preventDefault();
-          let delivery = $("#askForLocation").val();
-    
-          if(delivery === "Yes") {
-            $("#message").text("Your shipping cost is Ksh " + shippingCost);
-            $("#confirmAlert").show();
-            $("#locate").show();
-            $("#userInfo").hide();
-            $(this).hide();
-            $("#ask").hide();
-          }
-          else if (delivery == "No") {
-            $("#userInfo").show();
-            $("#locate").hide();
-            $(this).hide();
-            $("#ask").hide();
-          }
+        //Confirmation for delivery option
+
+        $("form#data").submit(function (event) {
+            event.preventDefault();
+            let delivery = $("#askForLocation").val();
+
+            if (delivery === "Yes") {
+                $("#message").text("Your shipping cost is Ksh " + deliveryCost);
+                $("#data").show();
+                $("#locateUser").show();
+                $("#Info").hide();
+                $(this).hide();
+                $("#location").hide();
+            } else if (delivery == "No") {
+                $("#Info").show();
+                $("#locateUser").hide();
+                $(this).hide();
+                $("#location").hide();
+            }
         })
-    
+
         //capture details from user with no delivery
 
-        $("#detailsButton").click(function(event) {
-          event.preventDefault();
-          let userName= $("input#nameOne").val();
-          let phone = $("input#phone1").val();
-          if (userName.length === 0 || phone.length === 0) {
-            alert("Enter all fields before submitting!!!")
-          }
-          else {
-            $("#checkoutUser1").show();
-            $(this).hide();
-            $("#userInfo").hide();
-          }
-          
-          //show order summary on checkout without delivery
-
-          $("#checkout1").click(function(event) {
+        $("#detailsButton").click(function (event) {
             event.preventDefault();
-            $("#aggregatePriceOne").text(total);
-            $("#userName1").text(userName);
-            $("#phoneOne").text(phone);
-            $("#checkouAlertOne").show();
-            $(this).hide();
-          })
+            let userName = $("input#nameOne").val();
+            let phone = $("input#phone1").val();
+            if (userName.length === 0 || phone.length === 0) {
+                alert("Enter all fields before submitting!!!")
+            } else {
+                $("#checkoutUser1").show();
+                $(this).hide();
+                $("#Info").hide();
+            }
+
+            //show order summary on checkout without delivery
+
+            $("#checkout1").click(function (event) {
+                event.preventDefault();
+                $("#priceOne").text(total);
+                $("#usser").text(userName);
+                $("#userPhoneOne").text(phone);
+                $("#popupOne").show();
+                $(this).hide();
+            })
         })
-    
+
         //Receive inputs from customer with delivery
 
-        $("#locationButton").click(function(event) {
-          event.preventDefault();
-          const shippingLocation = $("input#shippingLocation").val();
-          let userName = $("input#name").val();
-          let phone = $("input#phone").val();
-    
-          if (shippingLocation.length === 0 || userName.length ===0 || phone.length ===0) {
-            alert ("Enter all fields before submitting!!!")
-          }
-          else {
-            $("#areaMessage").text("Your shipping location is " + shippingLocation +". This order will be delivered to your location after you checkout.");
-            $("#locationAlert").show();
-            $("#checkOut").show();
-            $("#locate").hide();
-            $(this).hide();
-          }
-
-          //Display order summary on checkout with delivery
-
-          $("#checkoutButton").click(function(event) {
+        $("#deliveryButton").click(function (event) {
             event.preventDefault();
-            let totalOrderCost = total + shippingCost;
-            $("#userNameTwo").text(userName);
-            $("#phoneTwo").text(phone);
-            $("#aggregatePrice").text(total);
-            $("#shippingArea").text(shippingLocation);
-            $("#shippingCost").text(shippingCost);
-            $("#totalAmount").text(totalOrderCost);
-            $("#checkouAlert").show();
-            $(this).hide();
-          })
+            const deliveryLocation = $("input#deliveryLocation").val();
+            let userName = $("input#name").val();
+            let phone = $("input#phone").val();
+
+            if (deliveryLocation.length === 0 || userName.length === 0 || phone.length === 0) {
+                alert("Enter all fields before submitting!!!")
+            } else {
+                $("#message2").text("Your shipping location is " + deliveryLocation + ". This order will be delivered to your location after you checkout.");
+                $("#alert").show();
+                $("#checkOut").show();
+                $("#locateUser").hide();
+                $(this).hide();
+            }
+
+            //Display order summary on checkout with delivery
+
+            $("#checkoutButton").click(function (event) {
+                event.preventDefault();
+                let totalOrderCost = total + deliveryCost;
+                $("#nameTwo").text(userName);
+                $("#userPhoneTwo").text(phone);
+                $("#price").text(total);
+                $("#shippingLocation").text(deliveryLocation);
+                $("#deliveryCost").text(deliveryCost);
+                $("#totalSum").text(totalOrderCost);
+                $("#checkoutAlert").show();
+                $(this).hide();
+            })
         })
-      });
+    });
 
 });
