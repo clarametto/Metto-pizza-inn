82f7e0e2aba733407a8b40f1ab72d78d15f45d1c
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-09-19 23:17:07 +0300

    modified what we do and our brand sections and style it on css

--- js/script.js
+++ js/script.js
@@ -1,28 +1,23 @@
+    // form
+    
+    function contactForm(){
+    var name=document.getElementById('name').value;
+    var email=document.getElementById('email').value;
+    var message=document.getElementById('message').value;
+
+    if (name !== "" && email !== "" && message !== ""){
+        alert(name+ ' we have received your message. Thank you for reaching out to us.');
+    } else if(name !== "" || email !== "" || message !== ""){
+        alert('Fill everything, please.')
+    }
+};
+
 $(document).ready(function () {
 
     $(".main").click(function () {
         $(this).children("#design-p").slideToggle(2000);
         $(this).children("#design").toggle(2000);
     });
-
-    // form
-
-    $(".form").submit(function () {
-        var name = document.getElementById("mce-NAME").value
-        var email = document.getElementById("mce-EMAIL").value
-        var message = document.getElementById("mce-MESSAGE").value
-        if (name == "" || name == null) {
-            alert("Name can not be empty")
-        } else if (email == "" || email == null) {
-            alert("Email can not be empty")
-        } else if (message == "" || message == null) {
-            alert("message can not be empty")
-        } else {
-            alert(" We have received your message " + name + ". Thank you for reaching out to us")
-        }
-
-
-    })
 
 });
 
